a30c479ac64d43a3e11e3ad1f4ba9f2aca5926b7
Luciano Bueno <glamero@gmail.com>
2016-05-26 00:08:19 -0300

    bump

--- apps/dns/assets/index.js
+++ apps/dns/assets/index.js
@@ -51,63 +51,19 @@ head.ready('jsonp', function(){
 				}
 			});
 				
-			//self.zones.extend({ rateLimit: 0 });
+
 		};
 		
-		//DNSBodyModel = {
-			//dns: new DNSModel()
-		//}
-		//mainBodyModel= {
-			//dns: new DNSModel()
-		//}
-		//mainBodyModel.dns = ko.observable({
-			//zones : [
-				//{zone: "aamepsi.com.ar"},
-				//{zone: "academiadederecho.com.ar"}
-			//]
-		//});
-		
-		//var DNSModel = {
-			
-			//zones: ko.observableArray([
-				//{zone: "aamepsi.com.ar"},
-				//{zone: "academiadederecho.com.ar"}
-			//])
-
-		//};
 		
 		console.log(mainBodyModel.dns());
 		
 		if(mainBodyModel.dns() == null){
-			//mainBodyModel.dns(DNSModel);
+
 			mainBodyModel.dns(new DNSModel());
-			//console.log('body');
-			//console.log(mainBodyModel);
-			////ko.tasks.runEarly();
-			
-			//ko.cleanNode(document.getElementById("main-body"));
-			//ko.applyBindings(mainBodyModel, document.getElementById("main-body"));
+
 			console.log('DNS binding applied');
-			//mainBodyModel.dns().zones([
-				//{zone: "aamepsi.com.ar"},
-				//{zone: "academiadederecho.com.ar"}
-			//]);
-			//mainBodyModel.dns().zones.push({zone: "aamepsi.com.ar"});
+
 		}
-		
-		//dns = new DNSModel();
-		//ko.applyBindings(); 
-		
-		
-			//ko.applyBindings({
-					//zones: [
-						//{zone: "aamepsi.com.ar"},
-						//{zone: "academiadederecho.com.ar"}
-					//]
-			//});
-		//ko.applyBindings(mainBodyModel, document.getElementById("main-body"));
-		
-		//ko.applyBindings(DNSBodyModel, document.getElementById("DNSBodyModel"));
 			
 	});
 });
